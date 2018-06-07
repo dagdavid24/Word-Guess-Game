b53177df117ba007eece4af86b06940ccf7e445b
Crixus <dagdavid24@yahoo.com>
2018-06-07 17:14:31 -0400

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//create array of words
+var words = ["chelsea", "manchester united", "liverpool", "tottenham", 
+"real madrid", "barcelona", "arsenal", "juventus", "bayern munich", "psg", "porto"];
+//choose words randomly
+var randomWord = words[Math.floor(Math.random()*words.length)];
+console.log(randomWord);
+//create underscores based on length of word chosen
+let underScore = () => {
+    for (var i=0; i < words.length; i++){
+        
+    }
+    return underScore;
+}
+console.log;
+//get user guess
+/*check if guess is right.
+    - If right push into right array
+    - If wrong push into wrong array */
