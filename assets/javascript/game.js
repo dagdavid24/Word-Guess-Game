6d63e82384e6f0ba3620e512bcf9d951aabfc52f
Crixus <dagdavid24@yahoo.com>
2018-06-07 18:17:35 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,35 @@
-//create array of words
-var words = ["chelsea", "manchester united", "liverpool", "tottenham", 
+
+var team = ["chelsea", "manchester united", "liverpool", "tottenham", 
 "real madrid", "barcelona", "arsenal", "juventus", "bayern munich", "psg", "porto"];
-//choose words randomly
-var randomWord = words[Math.floor(Math.random()*words.length)];
+
+var randomWord = team[Math.floor(Math.random()*team.length)];
 console.log(randomWord);
-//create underscores based on length of word chosen
-let underScore = () => {
-    for (var i=0; i < words.length; i++){
-        
+
+var underScore =[];
+function createUnderScore() {
+    for (var i=0; i < randomWord.length; i++){
+        underScore.push("_");
     }
     return underScore;
 }
-console.log;
-//get user guess
+console.log(createUnderScore());
+
+var correctGuess = [];
+var wrongGuess = [];
+var count;
+document.addEventListener('keypress', (event) => {
+    var keycode = event.keyCode;
+    var guessWord = String.fromCharCode(keycode);
+    if (randomWord.indexOf(guessWord)> -1) {
+        correctGuess.push(guessWord);
+        console.log(correctGuess);
+    }
+    else {
+        wrongGuess.push(guessWord);
+        console.log(wrongGuess);
+    }
+    
+})
 /*check if guess is right.
     - If right push into right array
     - If wrong push into wrong array */
