c9d8bd941f752209600ba6bcb55021dddbfef273
Crixus <dagdavid24@yahoo.com>
2018-06-07 23:15:56 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var team = ["chelsea", "manchester united", "liverpool", "tottenham",
 var randomWord = team[Math.floor(Math.random()*team.length)];
 console.log(randomWord);
 
-var underScore =[];
+var underScore = [];
 function createUnderScore() {
     for (var i=0; i < randomWord.length; i++){
         underScore.push("_");
@@ -16,20 +16,29 @@ console.log(createUnderScore());
 
 var correctGuess = [];
 var wrongGuess = [];
-var count;
+var count = 0;
+var guess = document.getElementById('guess');
+var letterguess = document.getElementById('letterguess');
+var letterguess = document.getElementById('letterguess');
+
 document.addEventListener('keypress', (event) => {
     var keycode = event.keyCode;
     var guessWord = String.fromCharCode(keycode);
     if (randomWord.indexOf(guessWord)> -1) {
         correctGuess.push(guessWord);
-        console.log(correctGuess);
+        underScore[randomWord.indexOf(guessWord)] = guessWord;
+        //guess[i].innerHTML = underScore.join("");
+        //letterguess[i].innerHTML = correctGuess;
+        console.log(correctGuess); 
     }
     else {
         wrongGuess.push(guessWord);
         console.log(wrongGuess);
     }
-    
+
 })
+//.innerHTML = createUnderScore();
+
 /*check if guess is right.
     - If right push into right array
     - If wrong push into wrong array */
